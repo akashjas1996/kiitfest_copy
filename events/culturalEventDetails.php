2a8ba23784612c87d18d8581ef623195b751ecca
xoumi <xoumi712@gmail.com>
2019-01-26 22:32:04 +0530

    Cultural Templates added.

---
+++ events/culturalEventDetails.php
@@ -0,0 +1,39 @@
+<script>
+    var eventNames = {
+            "901": "Frozen In Time",
+            "902": "Mismatch - The Masterchef Event",
+            "903": "Open Sesame",
+            "904": "Zara Nachke Dikha",
+            "905": "Tell A Tale",
+            "906": "Shabd",
+            "907": "Spotlight",
+            "908": "Isle To Glory",
+            "909": "Instant-o-fie",
+            "910": "FUEGO",
+            "911": "CHITRA-VI-CHITRA-CHITRAM",
+            "912": "Alankaar",
+            "913": "Dekh Tamasha Dekh",
+            "914": "Melange",
+            "915": "Tulika",
+            "916": "Illustory"
+    }
+
+    var eventDetails = {
+        "901": `<h1>Frozen In Time</h1>`
+        "902": `<h1>Mismatch - The Masterchef Event</h1>`
+        "903": `<h1>Open Sesame</h1>`
+        "904": `<h1>Zara Nachke Dikha</h1>`
+        "905": `<h1>Tell A Tale</h1>`
+        "906": `<h1>Shabd</h1>`
+        "907": `<h1>Spotlight</h1>`
+        "908": `<h1>Isle To Glory</h1>`
+        "909": `<h1>Instant-o-fie</h1>`
+        "910": `<h1>FUEGO</h1>`
+        "911": `<h1>CHITRA-VI-CHITRA-CHITRAM</h1>`
+        "912": `<h1>Alankaar</h1>`
+        "913": `<h1>Dekh Tamasha Dekh</h1>`
+        "914": `<h1>Melange</h1>`
+        "915": `<h1>Tulika</h1>`
+        "916": `<h1>Illustory</h1>`
+    }
+</script>
