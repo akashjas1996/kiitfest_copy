57c4c6016836d064f64679c9b3dc521096d75306
Akash Jaiswal <akashjas1996@gmail.com>
2019-01-29 01:07:47 +0530

    Update technical.php

--- technical.php
+++ technical.php
@@ -90,6 +90,7 @@ echo '<script> var isLogin = '. $isLogin. ';</script>'
 
       <div class="eventDetailsContainer">
         <div class="eventContent"></div>
+        <p style="color:white"> Choose even to see details. </p>
           <!-- Event detailsfor each event get rendered here by js -->
       </div>
 
