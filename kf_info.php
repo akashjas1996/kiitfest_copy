b1f5ab68e0bf8ac77dc82724049b05c36c096ac4
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-05 21:18:22 +0530

    kf_info page update

--- kf_info.php
+++ kf_info.php
@@ -1,57 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet"> 
-</head>
-
-<?php
-$servername = "51.68.139.41";
-$username = "kiitfest";
-$password = "hi9jkH27Gb1sEkRj";
-$dbname = "kiitfest_5";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$kf_id = $_POST["kiitfest_id"];
-echo $kf_id;
-
-$sql = "SELECT name, email, phone FROM participants_participant WHERE kf_id='$kf_id'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        //echo "Name : " . $row["name"]. " - email: " .$row["email"]."<br>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
-echo '
-<body>
-<div class="id-card-wrapper">
-  <div class="id-card">
-    <div class="profile-row">
-      <div class="dp">
-        <div class="dp-arc-outer"></div>
-        <div class="dp-arc-inner"></div>
-        <img src="https://via.placeholder.com/420x420">
-      </div>
-      <div class="desc">
-        <h1>'.$row['name'].'</h1>
-            <p>Strength: Ironman Suit</p>
-            <p>Weakness: None</p>
-            <p>Known as: Iron Man</p>
-      </div>
-    </div>
-  </div>
-</div>
-</body>
-
+	<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
 <style>
   body {
   margin:0px;
@@ -153,4 +103,59 @@ echo '
   margin: 0px;
 }
 </style>
-</html>' ?>
+</head>
+<body> 
+
+
+<?php
+$servername = "51.68.139.41";
+$username = "kiitfest";
+$password = "hi9jkH27Gb1sEkRj";
+$dbname = "kiitfest_5";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$kf_id = $_POST["kiitfest_id"];
+echo $kf_id;
+
+$sql = "SELECT name, email, phone FROM participants_participant WHERE kf_id='$kf_id'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //echo "Name : " . $row["name"]. " - email: " .$row["email"]."<br>";
+        echo '
+        
+		<div class="id-card-wrapper">
+  <div class="id-card">
+    <div class="profile-row">
+      <div class="dp">
+        <div class="dp-arc-outer"></div>
+        <div class="dp-arc-inner"></div>
+        <img src="https://via.placeholder.com/420x420">
+      </div>
+      <div class="desc">
+        <h1>Tony Stark</h1>
+            <p>Strength: Ironman Suit</p>
+            <p>Weakness: None</p>
+            <p>Known as: Iron Man</p>
+      </div>
+    </div>
+  </div>
+</div>
+</body>'
+    }
+} else {
+    echo "0 results";
+}
+$conn->close(); ?>
+
+
+
+</body>
+</html>
