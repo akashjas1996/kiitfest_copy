6ec22393c3309aca00187f6f5f7623a9dc2fa682
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-05 21:11:08 +0530

    Update kf_info.php

--- kf_info.php
+++ kf_info.php
@@ -42,7 +42,7 @@ echo '
         <img src="https://via.placeholder.com/420x420">
       </div>
       <div class="desc">
-        <h1><?php echo $row["name"];?></h1>
+        <h1>'.$row['name'].'</h1>
             <p>Strength: Ironman Suit</p>
             <p>Weakness: None</p>
             <p>Known as: Iron Man</p>
