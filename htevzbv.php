7a82f326f9f54f298055ec4bbe56706344f04c35
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-05 15:43:11 +0530

    Update htevzbv.php

--- htevzbv.php
+++ htevzbv.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, name, lastname FROM participants_participants";
+$sql = "SELECT name, email, phone FROM participants_participant";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "Name : " . $row["name"]. " - email: " ."<br>";
     }
 } else {
     echo "0 results";
