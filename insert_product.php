8f75fa9d1a49e54cb95c27270c919f49a4b2825e
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-06 00:32:35 +0530

    Update insert_product.php

--- insert_product.php
+++ insert_product.php
@@ -1,4 +1,20 @@
 <?php
+
+$servername = "51.68.139.41";
+$username = "kiitfest";
+$password = "hi9jkH27Gb1sEkRj";
+$dbname = "kiitfest_5";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$kf_id = $_POST["kiitfest_id"];
+echo $kf_id;
+
+
 $image = addslashes(file_get_contents($_FILES['image']['tmp_name'])); //SQL Injection defence!
 $image_name = addslashes($_FILES['image']['name']);
 $sql = "INSERT INTO `product_images` (`id`, `image`, `image_name`) VALUES ('1', '{$image}', '{$image_name}')";
