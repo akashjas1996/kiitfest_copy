1f1e83e3a60b002c020d945505e7ffd48448026e
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-05 15:59:18 +0530

    Update htevzbv.php

---
+++ htevzbv.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "51.68.139.41";
+$username = "kiitfest";
+$password = "hi9jkH27Gb1sEkRj";
+$dbname = "kiitfest_5";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT name, email, phone FROM participants_participant";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name : " . $row["name"]. " - email: " .$row["email"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
