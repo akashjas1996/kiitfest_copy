e0ba7b7fccdc88d6d7e5b3fe5d9c7d5b0e1a6ce1
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-05 22:16:25 +0530

    Update kf_info.php

--- kf_info.php
+++ kf_info.php
@@ -130,10 +130,8 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         //echo "Name : " . $row["name"]. " - email: " .$row["email"]."<br>";
         if($row["payment_complete"]==1){
-        	$color="red";
-        }
-        echo ' 
-        <div class="id-card" background-color='echo $color'>
+        	echo ' 
+        <div class="id-card" background-color="red">
     <div class="profile-row">
       <div class="dp">
         <div class="dp-arc-outer"></div>
@@ -150,6 +148,28 @@ if ($result->num_rows > 0) {
     </div>
   </div>
 ';
+        }
+        else{
+        	echo "Hello"
+        }
+        /*echo ' 
+        <div class="id-card" background-color='echo $color'>
+    <div class="profile-row">
+      <div class="dp">
+        <div class="dp-arc-outer"></div>
+        <div class="dp-arc-inner"></div>
+        <img src="https://via.placeholder.com/420x420">
+      </div>
+      <div class="desc">
+        <h1>'.$row['name'].'</h1>
+            <p>KFID:'.$row['kf_id'].'</p>
+            <p>Email:'.$row['email'].'</p>
+            <p>Payment:'.$row['payment_complete'].'</p>
+            <p>Payment:'.$color.'</p>
+      </div>
+    </div>
+  </div>
+';*/
     }
 } else {
     echo "0 results";
