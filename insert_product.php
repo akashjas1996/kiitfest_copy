59d595bcfd63d16f5aa04528cdeab15d9336931a
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-06 00:40:00 +0530

    Update insert_product.php

--- insert_product.php
+++ insert_product.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 $image = addslashes(file_get_contents($_FILES['image']['tmp_name'])); //SQL Injection defence!
 $image_name = addslashes($_FILES['image']['name']);
 $sql = "INSERT INTO `product_images` (`id`, `image`, `image_name`) VALUES ('1', '{$image}', '{$image_name}')";
-if (!mysql_query($sql)) { // Error handling
-    echo "Something went wrong! :("; 
+if (!mysqli_query($sql)) { // Error handling
+    echo "Something went wrong!"; 
 }
 ?>
