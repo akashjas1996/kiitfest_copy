7acc90639a5b65e96b2f7572b6c7e80e71507de1
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-05 15:36:49 +0530

    sponsor

---
+++ htevzbv.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "51.68.139.41";
+$username = "kiitfest";
+$password = "hi9jkH27Gb1sEkRj";
+$dbname = "kiitfest_5";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, name, lastname FROM participants_participants";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
