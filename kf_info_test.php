6cecb971222b32d9842b91c6263786c45f17ac3b
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-06 09:03:17 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -12,4 +12,12 @@ if ($conn->connect_error) {
 } 
 $kf_id = $_POST["kiitfest_id"];
 echo $kf_id;
+
+$sql = "SELECT * FROM participants_participant WHERE kf_id='$kf_id'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name : " . $row["name"]. " - email: " .$row["email"]."<br>";
   ?>
