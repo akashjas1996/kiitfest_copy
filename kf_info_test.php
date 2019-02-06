ff554a619a71ef04c3f95f079c48197ca0cf4aa2
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-06 09:01:51 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -1,4 +1,15 @@
 <?php
-  $kf_id = $_POST["kiitfest_id"];
-  echo $kf_id;
+  $servername = "51.68.139.41";
+  $username = "kiitfest";
+  $password = "hi9jkH27Gb1sEkRj";
+  $dbname = "kiitfest_5";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$kf_id = $_POST["kiitfest_id"];
+echo $kf_id;
   ?>
