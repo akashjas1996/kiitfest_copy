82d0da2b4b4acd58d3fc66d862277a2e7d79e12d
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-06 09:43:58 +0530

    Update kf_info.php

--- kf_info.php
+++ kf_info.php
@@ -163,9 +163,17 @@ if ($result->num_rows > 0) {
             <p>Payment:'.$row['roll_no'].'</p>
       </div>
     </div>
-  </div>'
+  </div>
 <br><br>
-<input type="button"> ENROLL </input>
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" value="Mickey">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Mouse">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> '
   ;
         }
         else
