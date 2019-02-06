5b7252f30b4380ece0a33f008782a141a93132f0
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-06 13:44:47 +0530

    Update kf_info.php

--- kf_info.php
+++ kf_info.php
@@ -165,7 +165,7 @@ if ($result->num_rows > 0) {
       </div>
     </div>
     <form action="" method="POST">
-    <input type="text" name="kiitfestid" value="'.$row['kf_id'].'">
+    <input  type="text" name="kiitfestid" value="'.$row['kf_id'].'">
   <input type="text" name="barcode">
   <br><br>
   <input type="submit" value="save">
@@ -206,6 +206,11 @@ if(isset($_POST['barcode'])){
 	$barc = $_POST["barcode"];
 	$kf = $_POST["kiitfestid"];
 	//echo "You clicked on: ".$row['kf_id'];
+	$sql_u = "SELECT * FROM kf_barcode WHERE kfid=`$kf`";
+	$result = $conn->query($sql_u);
+	if($result->num_rows>0){
+		alert("Participant has already received the ID Card.");
+	}
 	$sql = "INSERT INTO kf_barcode(kfid, barcode)VALUES('$kf', '$barc')";
 		$result = $conn->query($sql);
 		if($result)
