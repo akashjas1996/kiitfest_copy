affa1663cd7ce06f9e48b6e6b717789f183a8af1
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 02:15:57 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-  <button class="btn btn-primary" href="#<?php echo '$start+100'?>">V</button>
+  <button class="btn btn-primary" href=col"#<?php echo '$start+100'?>">V</button>
   <?php $start=$start+100; ?>
 <table class="table table-bordered table-hover table-dark">
   <thead>
