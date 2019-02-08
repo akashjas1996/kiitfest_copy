63692acb0b0b9f3a5d09721a1177e7bbc61a0edb
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 01:35:13 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -1,3 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sl</th>
+      <th scope="col">KF ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Roll No</th>
+    </tr>
+  </thead>
+
 <?php
 error_reporting(E_ALL);
   $servername = "51.68.139.41";
@@ -17,9 +33,33 @@ $result = $conn->query($sql);
 $sl=1;
 while($row = $result->fetch_assoc()) {
       if($row["payment_complete"]!=1){
-        echo ' 
+        echo '
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>'
+        /*echo ' 
         <h1></h1>
-            <p>'.$sl.'  '.$row['kf_id'].'  '.$row['name'].'  '.$row['email'].'  '.$row['payment_complete'].'  '.$row['phone'].'</p>';} 
+            <p>'.$sl.'  '.$row['kf_id'].'  '.$row['name'].'  '.$row['email'].'  '.$row['payment_complete'].'  '.$row['phone'].'</p>';*/} 
             $sl=$sl+1;
       }
 ?>
+
+</body>
