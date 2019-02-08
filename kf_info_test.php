a9898d2b2fbc2a15dc48aef5fc3b85a54af6aa86
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 01:46:00 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -4,7 +4,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-<table class="table.dark">
+<table class="table table-bordered">
   <thead>
     <tr>
       <th scope="col">Sl</th>
