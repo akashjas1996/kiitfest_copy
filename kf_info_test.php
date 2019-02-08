66aec85ea612276c1610d2d43264118999c18e26
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 02:52:54 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -2,64 +2,13 @@
 <html>
 <head>
   <style>
-  @media
-    only screen 
-    and (max-width: 760px), (min-device-width: 768px) 
-    and (max-device-width: 1024px)  {
 
-    /* Force table to not be like tables anymore */
-    table, thead, tbody, th, td, tr {
-      display: block;
-    }
-
-    /* Hide table headers (but not display: none;, for accessibility) */
-    thead tr {
-      position: absolute;
-      top: -9999px;
-      left: -9999px;
-    }
-
-    tr {
-      margin: 0 0 1rem 0;
-    }
-      
-    tr:nth-child(odd) {
-      background: #ccc;
-    }
-    
-    td {
-      /* Behave  like a "row" */
-      border: none;
-      border-bottom: 1px solid #eee;
-      position: relative;
-      padding-left: 50%;
-    }
-
-    td:before {
-      /* Now like a table header */
-      position: absolute;
-      /* Top/left values mimic padding */
-      top: 0;
-      left: 6px;
-      width: 45%;
-      padding-right: 10px;
-      white-space: nowrap;
-    }
-
-    /*
-    Label the data
-    You could also use a data-* attribute and content for this. That way "bloats" the HTML, this way means you need to keep HTML and CSS in sync. Lea Verou has a clever way to handle with text-shadow.
-    */
-    td:nth-of-type(1):before { content: "Sl"; }
-    td:nth-of-type(2):before { content: "Name"; }
-    td:nth-of-type(3):before { content: "Institute"; }  
-    td:nth-of-type(5):before { content: "."; }
-  }
 </style>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
 <body>
-<table class="table table-bordered table-hover table-dark table-striped">
+<table class="table table-bordered table-hover table-dark table-striped table-condensed">
   <thead>
     <tr>
       <th scope="col">Sl</th>
@@ -96,7 +45,8 @@ while($row = $result->fetch_assoc()) {
       <td>'.$row['kf_id'].'</td>
       <td>'.$row['name'].'</td>
       <td>'.$row['institution'].'</td>
-      <td ><button class="btn btn-primary"><a href="tel://+91'.$row['phone'].'">'.$row['phone'].'</button></td>
+      
+      <td><a href="tel://+91'.$row['phone'].'"><i class="fas fa-call" style="font-size:24px;"></i></td>
     </tr>';
         /*echo ' 
         <h1></h1>
