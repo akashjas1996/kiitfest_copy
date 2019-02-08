31d6975f276655914a4abe6302906649f3e7aec3
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 01:58:31 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -41,7 +41,7 @@ while($row = $result->fetch_assoc()) {
       <td>'.$row['kf_id'].'</td>
       <td>'.$row['name'].'</td>
       <td>'.$row['institution'].'</td>
-      <button><td><a href="tel://+91'.$row['phone'].'">'.$row['phone'].'</td></button>
+      <td><button><a href="tel://+91'.$row['phone'].'">'.$row['phone'].'</button></td>
     </tr>';
         /*echo ' 
         <h1></h1>
