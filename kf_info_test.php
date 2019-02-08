5c267d21df2540bdd213aac27e3d92b94b0bc3ef
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 00:43:57 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -18,6 +18,6 @@ while($row = $result->fetch_assoc()) {
       if($row["payment_complete"]!=1){
         echo ' 
         <h1>'.$row['name'].'</h1>
-            <p>KFID : '.$row['kf_id'].'Email: '.$row['email'].$row['kf_id'].'</p>';}
+            <p>KFID : '.$row['kf_id'].'Email: '.$row['email'].'payment : '.$row['payment_complete'].'</p>';}
       }
 ?>
