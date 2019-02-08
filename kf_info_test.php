f91e991b5832f6fd824be16a5a2e586e00e98618
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 01:06:11 +0530

    Update kf_info_test.php

--- kf_info_test.php
+++ kf_info_test.php
@@ -14,10 +14,12 @@ if ($conn->connect_error) {
 //echo "Connected successfully";
 $sql = "SELECT kf_id, name, roll_no FROM participants_participant"; 
 $result = $conn->query($sql);
+$sl=1;
 while($row = $result->fetch_assoc()) {
       if($row["payment_complete"]!=1){
         echo ' 
         <h1></h1>
-            <p>'.$row['kf_id'].'  '.$row['name'].'  '.$row['email'].'  '.$row['payment_complete'].'  '.$row['phone'].'</p>';}
+            <p>'.'$sl'.$row['kf_id'].'  '.$row['name'].'  '.$row['email'].'  '.$row['payment_complete'].'  '.$row['phone'].'</p>';} 
+            $sl++;
       }
 ?>
