866ac2036a7b375effdd74740a160e0298e44e47
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 12:17:30 +0530

    Update login_test.php

--- login_test.php
+++ login_test.php
@@ -7,8 +7,8 @@ $db_name="kiitfest_5"; // Database name
 $tbl_name="volunteer_db"; // Table name 
 
 // Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
+mysqli_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysqli_select_db("$db_name")or die("cannot select DB");
 
 // username and password sent from form 
 $myusername=$_POST['myusername']; 
@@ -17,14 +17,14 @@ $mypassword=$_POST['mypassword'];
 // To protect MySQL injection (more detail about MySQL injection)
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
+$myusername = mysqli_real_escape_string($myusername);
+$mypassword = mysqli_real_escape_string($mypassword);
 
 $sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-$result=mysql_query($sql);
+$result=mysqli_query($sql);
 
 // Mysql_num_row is counting table row
-$count=mysql_num_rows($result);
+$count=mysqli_num_rows($result);
 
 // If result matched $myusername and $mypassword, table row must be 1 row
 
