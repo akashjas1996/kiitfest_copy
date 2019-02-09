51942c28c92fb8cbc4e1008e81105012057e9cfe
Akash Jaiswal <akashjas1996@gmail.com>
2019-02-09 12:59:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -416,7 +416,47 @@ s0.parentNode.insertBefore(s1,s0);
     <i class="fas fa-times close"></i>
   </div>
   -->
-  
+  <script>
+  	
+  	$('.responsive').slick({
+  dots: true,
+	prevArrow: $('.prev'),
+	nextArrow: $('.next'),
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+  </script>
   <script src="js/common.js"></script>
   <script src="js/index.js"></script>
 </body>
