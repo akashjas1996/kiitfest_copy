0274f0b6686081b355e13ce9c387573ac99febdc
Aayush Bibhuti <aayush.server@gmail.com>
2019-02-09 13:48:49 +0530

    server changes

---
+++ app/app_connect.php
@@ -0,0 +1,22 @@
+<?php
+define('hostname', '51.68.139.41');
+define('user', 'kiitfest');
+define('password', 'hi9jkH27Gb1sEkRj');
+define('db_name', 'kiitfest_5');
+
+class DB_Connection {
+		
+    private $connect;
+    function __construct() {
+        $this->connect = mysqli_connect(hostname, user, password, db_name)
+        or die("Could not connect to db");
+        
+    }
+    
+    public function getConnection()
+    {
+        return $this->connect;
+    }
+}
+
+?>
