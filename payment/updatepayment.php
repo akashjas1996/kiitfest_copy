5c4786b7b0a56ce8b8e3e338969712e3e34181a9
Akshat Anurag <akshatanurag1998@gmail.com>
2019-02-14 18:38:43 +0530

    fixed pay

--- payment/updatepayment.php
+++ payment/updatepayment.php
@@ -12,7 +12,6 @@
                     "X-Auth-Token:887515a0f019663aaf413c97904f0cca"));
     $response = curl_exec($ch);
     curl_close($ch); 
-    echo json_encode($response);
     $pay_id = $response['payment']['payment_id'];
    // echo $pay_id;
    if($pay_id != NULL){
