f9b15ce92941721a136525d5791706b1110b8fe7
Akshat Anurag <akshatanurag1998@gmail.com>
2019-02-14 19:58:16 +0530

    final pay fix

--- payment/updatepayment.php
+++ payment/updatepayment.php
@@ -13,9 +13,10 @@
 
     $response = curl_exec($ch);
     curl_close($ch); 
-    echo json_encode($response);
+    json_encode($response);
+    $response = json_decode($response,true);
     $pay_id = $response['payment']['payment_id'];
-   // echo $pay_id;
+   //echo $pay_id;
    if($pay_id != NULL){
        $email = $_POST['email'];
      $query1 = "update participants_participant set payment_complete = 1,payment_id='$pay_id' where email= '$email'";
